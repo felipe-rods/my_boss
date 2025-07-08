28f6d0570eb625d3470a3e2c6ce5a7d91aa36fe1
felipe-rods <felipe.git@outlook.com>
2025-07-07 21:58:06 -0300

    Task views, base html and components and task html test

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from tasks.models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'description', 'deadline', 'priority')
+
+
+admin.site.register(Task, TaskAdmin)
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from . import models
 
-# Create your views here.
+
+class TaskListView(ListView):
+    model = models.Task
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
+
+
+class TaskCreateView(CreateView):
+    model = models.Task
+    template_name = 'task_create.html'
+    fields = ['title']
+    success_url = reverse_lazy('task_list')
+
+
+class TaskDetailView(DetailView):
+    model = models.Task
+    template_name = 'task_detail.html'
+
+
+class TaskUpdateView(UpdateView):
+    model = models.Task
+    template_name = 'task_detail.html'
+    fields = ['title']
+    success_url = reverse_lazy('task_list')
+
+
+class TaskDeleteView(DeleteView):
+    model = models.Task
+    template_name = 'task_delete.html'
+    success_url = reverse_lazy('task_list')
